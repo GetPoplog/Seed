fa2fead6305467171c58380174b47227bd1bb4f6
Stephen Leach <sfkleach@gmail.com>
2021-05-27 05:29:38 -0400

    Add in testing for supplied.corepop

--- find.sh
+++ find.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 # This script finds the most recent corepop that works on this architecture. It relies
 # on a very simple test to deternine if the corepop executable works - it runs it locally.
-# Ideally it would run it in firejail, if available.
+# TODO: Ideally it will run it in firejail, if available.
 OSNAME=`uname -s | tr '[:upper:]' '[:lower:]'`
 ARCH=`uname -m`
-for i in `ls -1r ${OSNAME}/${ARCH}/*.corepop`; do 
-	output=`$i ":sysexit()" 2>&1`
-	if [ -z "$output" ]
-       	then 
-		echo $i
-		break
-	fi 
+for i in "supplied.corepop" `ls -1r ${OSNAME}/${ARCH}/*.corepop`; do 
+    output=`./$i ":sysexit()" 2>&1`
+    if [ -z "$output" ]; then 
+        echo $i
+        break
+    fi 
 done
 # If no good executable can be found, exit with an error code.
 /bin/false
