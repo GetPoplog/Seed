db71ef8705b0060bc6acdc46b0dd7269a803aed0
Will Price <will.price94@gmail.com>
2021-07-30 17:13:07 +0100

    Add job to test RPM

---
+++ .circleci/scripts/test_rpm.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+SEED_DIR="${1:-$PWD}"
+
+dnf install -y python3 python3-pip
+pip3 install nose2
+cd "$SEED_DIR"
+dnf install -y _build/artifacts/poplog-*.x86_64.rpm
+cd systests
+nose2 -X
