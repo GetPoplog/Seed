3e83d466b8bbc068bc7426b37c6e4d6d85e2aa5b
Will Price <will.price94@gmail.com>
2021-07-30 22:11:11 +0100

    Install pytest and make in test_rpm.sh

--- .circleci/scripts/test_rpm.sh
+++ .circleci/scripts/test_rpm.sh
@@ -3,8 +3,8 @@ set -euxo pipefail
 
 SEED_DIR="${1:-$PWD}"
 
-dnf install -y python3 python3-pip
-pip3 install nose2
+dnf install -y python3 python3-pip make
+pip3 install pytest
 cd "$SEED_DIR"
 dnf install -y _build/artifacts/poplog-*.x86_64.rpm
 make test
