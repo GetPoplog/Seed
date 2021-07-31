04e96868615e657e735e298a515087ad5aa8e97c
Will Price <will.price94@gmail.com>
2021-07-31 20:17:36 +0100

    Renamed testing changelog files with TEST_ prefix

--- contributor_tools/tests/test_make_changelog.py
+++ contributor_tools/tests/test_make_changelog.py
@@ -2,13 +2,13 @@ from pathlib import Path
 import subprocess
 
 HERE = Path(__file__).absolute().parent
-CHANGELOG_YAML_PATH = HERE / 'CHANGELOG.yml'
+CHANGELOG_YAML_PATH = HERE / 'TEST_CHANGELOG.yml'
 MAKE_CHANGELOG_PATH = HERE.parent / 'make_changelog.py'
 
 def test_generating_markdown_changelog(tmp_path):
-    output_path = tmp_path / 'CHANGELOG.md'
+    output_path = tmp_path / 'TEST_CHANGELOG.md'
     subprocess.check_call(["python3", str(MAKE_CHANGELOG_PATH), str(CHANGELOG_YAML_PATH), str(output_path)])
-    with open(HERE / 'CHANGELOG.md', 'r') as f:
+    with open(HERE / 'TEST_CHANGELOG.md', 'r') as f:
         expected = f.read()
     assert output_path.exists()
     with open(output_path, 'r') as f:
@@ -16,9 +16,9 @@ def test_generating_markdown_changelog(tmp_path):
 
 
 def test_generating_latest_markdown_changelog(tmp_path):
-    output_path = tmp_path / 'CHANGELOG.md'
+    output_path = tmp_path / 'TEST_CHANGELOG.md'
     subprocess.check_call(["python3", str(MAKE_CHANGELOG_PATH), "--latest", str(CHANGELOG_YAML_PATH), str(output_path)])
-    with open(HERE / 'CHANGELOG-latest.md', 'r') as f:
+    with open(HERE / 'TEST_CHANGELOG-latest.md', 'r') as f:
         expected = f.read()
     assert output_path.exists()
     with open(output_path, 'r') as f:
@@ -26,9 +26,9 @@ def test_generating_latest_markdown_changelog(tmp_path):
 
 
 def test_generating_debian_changelog(tmp_path):
-    output_path = tmp_path / 'CHANGELOG.debian'
+    output_path = tmp_path / 'TEST_CHANGELOG.debian'
     subprocess.check_call(["python3",  str(MAKE_CHANGELOG_PATH), "--format", "debian", str(CHANGELOG_YAML_PATH), str(output_path)])
-    with open(HERE / 'CHANGELOG.debian', 'r') as f:
+    with open(HERE / 'TEST_CHANGELOG.debian', 'r') as f:
         expected = f.read()
     assert output_path.exists()
     with open(output_path, 'r') as f:
