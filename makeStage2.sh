5f759056f1c0b922570d6d416b299dda42bb1cae
Stephen Leach <sfkleach@gmail.com>
2021-08-07 15:10:48 +0100

    Quoting all variables

--- makeStage2.sh
+++ makeStage2.sh
@@ -42,7 +42,7 @@ cd $usepop/pop/x/src/
 # $usepop/pop/src/newpop -link -x=-xm -norsv
 # $usepop/pop/src/newpop -link -x=-xt -norsv
 
-mkdir -p ${BUILD_HOME}/environments
+mkdir -p "${BUILD_HOME}/environments"
 
 # We need to capture the Poplog environment for each build variant.
 echo_env() {
@@ -56,50 +56,50 @@ echo_env() {
 
 $usepop/pop/src/newpop -link -x=-xt -norsv
 
-echo_env "$usepop" > ${BUILD_HOME}/environments/xt
-echo_env "$usepop/pop/.." > ${BUILD_HOME}/environments/xt-cmp
-( cd ${BUILD_HOME}/environments && \
+echo_env "$usepop" > "${BUILD_HOME}/environments/xt"
+echo_env "$usepop/pop/.." > "${BUILD_HOME}/environments/xt-cmp"
+( cd "${BUILD_HOME}/environments" && \
   sed -e 's!\[//USEPOP//]/pop/pop![//USEPOP//]/pop/pop-xt!g' < xt | \
   sed -e 's!\[//USEPOP//]/pop/lib/psv/![//USEPOP//]/pop/lib/psv-xt/!g' > xt-new )
 
-mkdir -p $usepop/pop/pop-xt
-mkdir -p $usepop/pop/lib/psv-xt
-( cd $usepop/pop/pop; tar cf - . ) | ( cd $usepop/pop/pop-xt; tar xf - )
-( cd $usepop/pop/lib/psv; tar cf - . ) | ( cd $usepop/pop/lib/psv-xt; tar xf - )
+mkdir -p "$usepop"/pop/pop-xt
+mkdir -p "$usepop"/pop/lib/psv-xt
+( cd "$usepop"/pop/pop; tar cf - . ) | ( cd "$usepop"/pop/pop-xt; tar xf - )
+( cd "$usepop"/pop/lib/psv; tar cf - . ) | ( cd "$usepop"/pop/lib/psv-xt; tar xf - )
 
 
 ### motif ######################################################################
 
 $usepop/pop/src/newpop -link -x=-xm -norsv
 
-echo_env "$usepop" > ${BUILD_HOME}/environments/xm
-echo_env "$usepop/pop/.." > ${BUILD_HOME}/environments/xm-cmp
-( cd ${BUILD_HOME}/environments && \
+echo_env "$usepop" > "${BUILD_HOME}/environments/xm"
+echo_env "$usepop/pop/.." > "${BUILD_HOME}/environments/xm-cmp"
+( cd "${BUILD_HOME}/environments" && \
   sed -e 's!\[//USEPOP//]/pop/pop![//USEPOP//]/pop/pop-xm!g' < xm | \
   sed -e 's!\[//USEPOP//]/pop/lib/psv/![//USEPOP//]/pop/lib/psv-xm/!g' > xm-new )
 
-mkdir -p $usepop/pop/pop-xm
-mkdir -p $usepop/pop/lib/psv-xm
-( cd $usepop/pop/pop; tar cf - . ) | ( cd $usepop/pop/pop-xm; tar xf - )
-( cd $usepop/pop/lib/psv; tar cf - . ) | ( cd $usepop/pop/lib/psv-xm; tar xf - )
+mkdir -p "$usepop"/pop/pop-xm
+mkdir -p "$usepop"/pop/lib/psv-xm
+( cd "$usepop"/pop/pop; tar cf - . ) | ( cd "$usepop"/pop/pop-xm; tar xf - )
+( cd "$usepop"/pop/lib/psv; tar cf - . ) | ( cd "$usepop"/pop/lib/psv-xm; tar xf - )
 
 
 ### nox ########################################################################
 
 $usepop/pop/src/newpop -link -nox -norsv
 
-echo_env "$usepop" > ${BUILD_HOME}/environments/nox
-echo_env "$usepop/pop/.." > ${BUILD_HOME}/environments/nox-cmp
-( cd ${BUILD_HOME}/environments && \
+echo_env "$usepop" > "${BUILD_HOME}/environments/nox"
+echo_env "$usepop/pop/.." > "${BUILD_HOME}/environments/nox-cmp"
+( cd "${BUILD_HOME}/environments" && \
   sed -e 's!\[//USEPOP//]/pop/pop![//USEPOP//]/pop/pop-nox!g' < nox | \
   sed -e 's!\[//USEPOP//]/pop/lib/psv/![//USEPOP//]/pop/lib/psv-nox/!g' > nox-new )
 
 # Rename rather than copy.
-mv $usepop/pop/pop $usepop/pop/pop-nox
-mv $usepop/pop/lib/psv $usepop/pop/lib/psv-nox
+mv "$usepop"/pop/pop "$usepop"/pop/pop-nox
+mv "$usepop"/pop/lib/psv "$usepop"/pop/lib/psv-nox
 
 ################################################################################
 
 # Choose our default build variant.
-ln -sf pop-nox $usepop/pop/pop
-ln -sf psv-nox $usepop/pop/lib/psv
+ln -sf pop-nox "$usepop/pop/pop"
+ln -sf psv-nox "$usepop/pop/lib/psv"
