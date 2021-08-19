8323288e62a2411aab2a2a24097235e215e4c763
Will Price <will.price94@gmail.com>
2021-08-19 21:49:06 +0100

    Warn on no valid corepops

--- find.sh
+++ find.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+set -euo pipefail
+shopt -s nullglob
 # This script finds the most recent corepop that works on this architecture. It relies
 # on a very simple test to deternine if the corepop executable works - it runs it locally.
 # TODO: Ideally it will run it in firejail, if available.
@@ -12,4 +14,5 @@ for i in "supplied.corepop" `ls -1r ${OSNAME}/${ARCH}/*.corepop`; do
     fi 
 done
 # If no good executable can be found, exit with an error code.
+>&2 echo "No valid corepop found."
 /bin/false
