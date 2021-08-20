ab3ef7a5ce4da513785a18dceb750cb12734a705
Will Price <will.price94@gmail.com>
2021-08-20 22:43:58 +0100

    Refactor tests into python + add a few new ones

---
+++ systests/common.py
@@ -0,0 +1,31 @@
+import os
+import shlex
+import shutil
+import subprocess
+import tempfile
+from pathlib import Path
+from typing import List, Union
+
+
+POPLOG_BINARY_PATH = shutil.which("poplog")
+HERE = Path(__file__).absolute().parent
+
+
+def run_poplog_commander(args: Union[str, List[str]], extra_env=None) -> str:
+    if extra_env is None:
+        extra_env = dict()
+    env = dict(**os.environ, **extra_env)
+    if isinstance(args, str):
+        args = shlex.split(args)
+    completed_process = subprocess.run(
+        [POPLOG_BINARY_PATH] + args, capture_output=True, env=env
+    )
+    assert completed_process.returncode == 0
+    return completed_process.stdout.decode("utf-8").strip()
+
+
+def run_pop11_program(src: str) -> str:
+    with tempfile.NamedTemporaryFile(suffix=".p") as src_file:
+        src_file.write(src.encode("utf-8"))
+        src_file.flush()
+        return run_poplog_commander(["pop11", src_file.name])
--- systests/test_poplog_commander.py
+++ systests/test_poplog_commander.py
@@ -1,16 +1,55 @@
-#!/usr/bin/python3
-import os
-import re
-import glob
-import subprocess
-import pytest
-from pathlib import Path
+from common import run_poplog_commander
 
-here = Path( os.path.realpath( __file__ ) ).parent
 
-@pytest.mark.parametrize("in_file_name", glob.glob(f"{here.absolute()}/poplog_commander_tests/*.test.sh"))
-def test_poplog_commander(in_file_name):
-    expected_file_name = re.sub( r'\.test\.sh$', r'.expected.txt', in_file_name )
-    res = subprocess.getoutput( f"/bin/bash {in_file_name}" )
-    with open( expected_file_name, 'r' ) as exp:
-        assert exp.read().strip() == res.strip()
+class TestInterpreters:
+    def test_pop11(self):
+        assert run_poplog_commander("pop11 :1 + 2=>") == "** 3"
+
+    def test_clisp(self):
+        assert run_poplog_commander("clisp :(+ 1 2) ") == "3"
+
+    def test_ml(self):
+        assert run_poplog_commander("pml :print(1+2)") == "3"
+
+    def test_prolog(self):
+        assert run_poplog_commander("prolog :3 = 3.") == "yes"
+
+
+class TestExec:
+    def test_echo(self):
+        assert run_poplog_commander("exec echo hello") == "hello"
+
+    def test_setting_variables_in_shell_environment(self):
+        assert run_poplog_commander("HELLO=hi exec sh -c 'echo $HELLO'") == "hi"
+
+    def test_environment_variables_are_overwritten_in_pop11_environment_in_run_mode(self):
+        assert run_poplog_commander(
+            ["--run", "pop11", ":systranslate('popcom')=>"],
+            extra_env={"popcom": "/nosuchfile"},
+        ).endswith("poplog/V16/pop/com")
+
+    def test_cli_variables_are_not_overwritten_in_pop11_environment_in_run_mode(self):
+        # In run mode, a variable definition specified within the CLI args should be honoured.
+        assert run_poplog_commander(
+                ["--run", "popcom=/nosuchfile", "pop11", ":systranslate('popcom')=>"]
+        ) == "** /nosuchfile"
+
+    def test_cli_variables_are_visible_in_pop11_environment_in_dev_mode(self):
+        assert run_poplog_commander(
+            ["FOO=bar", "pop11", ":systranslate('FOO')=>"],
+        ) == "** bar"
+
+
+    def test_non_overwritten_environment_variables_are_visible_in_pop11_environment_in_run_mode(self):
+        assert run_poplog_commander(
+            ["--run", "pop11", ":systranslate('FOO')=>"],
+            extra_env={"FOO": "BAR"}
+        ).endswith("BAR")
+
+    def test_environment_variables_are_not_overwritten_in_pop11_environment_in_dev_mode(self):
+        assert run_poplog_commander(
+                ["pop11", ":maplist([%'popcom', 'FOO'%], systranslate)=>"],
+                extra_env={"popcom": "/nosuchfile", "FOO": "BAR"},
+        ) == "** [/nosuchfile BAR]"
+
+
