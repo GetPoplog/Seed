b6339b45cc0293a45e79d004b174b54663092797
Will Price <will.price94@gmail.com>
2021-08-21 10:11:54 +0100

    Add tests for invalid varspec and invalid command

--- systests/common.py
+++ systests/common.py
@@ -11,15 +11,19 @@ POPLOG_BINARY_PATH = shutil.which("poplog")
 HERE = Path(__file__).absolute().parent
 
 
-def run_poplog_commander(args: Union[str, List[str]], extra_env=None) -> str:
+def run_poplog_commander(args: Union[str, List[str]], extra_env=None) -> subprocess.CompletedProcess:
     if extra_env is None:
         extra_env = dict()
     env = {**os.environ, **extra_env}
     if isinstance(args, str):
         args = shlex.split(args)
-    completed_process = subprocess.run(
+    return subprocess.run(
         [POPLOG_BINARY_PATH] + args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env
     )
+
+
+def check_poplog_commander(args: Union[str, List[str]], extra_env=None) -> str:
+    completed_process = run_poplog_commander(args, extra_env=extra_env)
     stderr = completed_process.stderr.decode("utf-8")
     stdout = completed_process.stdout.decode("utf-8")
     err_msg = (
@@ -34,4 +38,4 @@ def run_pop11_program(src: str) -> str:
     with tempfile.NamedTemporaryFile(suffix=".p") as src_file:
         src_file.write(src.encode("utf-8"))
         src_file.flush()
-        return run_poplog_commander(["pop11", src_file.name])
+        return check_poplog_commander(["pop11", src_file.name])
