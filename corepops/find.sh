d34d7b04645372e65ed5dcb169b0239788a92f5c
Will Price <will.price94@gmail.com>
2021-08-22 21:21:44 +0100

    Fix corepop/find.sh shellcheck errors and allow-list base/* files

--- corepops/find.sh
+++ corepops/find.sh
@@ -3,12 +3,12 @@ set -euo pipefail
 # This script finds the most recent corepop that works on this architecture. It relies
 # on a very simple test to deternine if the corepop executable works - it runs it locally.
 # TODO: Ideally it will run it in firejail, if available.
-OSNAME=`uname -s | tr '[:upper:]' '[:lower:]'`
-ARCH=`uname -m`
+OSNAME="$(uname -s | tr '[:upper:]' '[:lower:]')"
+ARCH="$(uname -m)"
 
-for corepop in supplied.corepop ${OSNAME}/${ARCH}/*.corepop; do
+for corepop in supplied.corepop "${OSNAME}/${ARCH}"/*.corepop; do
     if [[ -f "$corepop" ]]; then
-        output=`./$corepop ":sysexit()" 2>&1`
+        output="$("./$corepop" ":sysexit()" 2>&1)"
         if [ -z "$output" ]; then
             echo "$corepop"
             exit 0
