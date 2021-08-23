246976bf4fbfe7aac4e2ff9e5682d2141a9ce5ce
Will Price <will.price94@gmail.com>
2021-08-23 18:12:16 +0100

    Fix mess corepops report generation made
    The corepop report was being injected into the top level readme, not the
    one in the corepops directory. I've rectified this and added some checks
    to ensure the start/end tags that are used to determine where to inject
    the table are present.
    Without these the script just repeatedly concatenates the whole readme
    to itself on each run... obviously not what we want!

--- corepops/.circleci/scripts/update-readme.sh
+++ corepops/.circleci/scripts/update-readme.sh
@@ -8,9 +8,15 @@ BEGIN_TAG="<!--BEGIN COREPOP_TEST_RESULTS-->"
 END_TAG="<!--END COREPOP_TEST_RESULTS-->"
 TABLE="$(cat "$TABLE_PATH")"
 
-# Copy everything before and including begin tag
-sed --null-data "s/\(.*${BEGIN_TAG}\n\).*/\1/" < "${README_PATH}" >> "$OUT_PATH"
-# Inject table
-echo "$TABLE" >> "$OUT_PATH"
-# Copy everything after and including end tag
-sed --null-data "s/.*\(${END_TAG}.*\)/\1/" <"${README_PATH}" >> "$OUT_PATH"
+# Error out if we don't have the correct tags in the README.
+grep "$BEGIN_TAG" "${README_PATH}" || { echo "Missing $BEGIN_TAG in $README_PATH"; exit 1; }
+grep "$END_TAG" "${README_PATH}" || { echo "Missing $END_TAG in $README_PATH"; exit 1; }
+
+{
+    # Copy everything before and including begin tag
+    sed --null-data "s/\(.*${BEGIN_TAG}\n\).*/\1/" < "${README_PATH}"
+    # Inject table
+    echo "$TABLE"
+    # Copy everything after and including end tag
+    sed --null-data "s/.*\(${END_TAG}.*\)/\1/" <"${README_PATH}"
+} >> "$OUT_PATH"
