99b5f8127189ba3b40f030b26b7670fb22c2ba42
Stephen Leach <sfkleach@gmail.com>
2021-08-23 22:11:01 +0100

    Add a check to verify that there are no new case-twinned files

---
+++ .circleci/scripts/check_for_casetwins.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+shopt -s globstar nullglob
+
+# We scan the Poplog tree looking for ordinary files whose paths differ
+# only by case e.g. 
+#   pop/packages/vedmail/auto/ved_respond.p
+#   pop/packages/vedmail/auto/ved_Respond.p
+# These files will cause difficulties if we ever move to a filing system
+# that is case-preserving but case-insensitive (Windows, MacOS HFS).
+# We count this as techdebt.
+
+# We have a list of known problems and we want to be warned if we find
+# that list is expanded.
+
+mkdir -p _build/poplog_base/
+(cd _build/poplog_base && find . -type f | tr 'A-Z' 'a-z' | sort | uniq --repeated) > _build/casetwins.txt
+cmp _build/casetwins.txt .circleci/scripts/known_casetwins.txt
