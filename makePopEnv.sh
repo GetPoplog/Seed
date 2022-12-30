d95526c23647c6a43ff4ed7b1184a83c8ecd543c
Stephen Leach <sfkleach@gmail.com>
2022-12-30 22:25:16 +0000

    Fix typo in original file

--- makePopEnv.sh
+++ makePopEnv.sh
@@ -15,7 +15,7 @@ pop_pop11="-$popsavelib/startup.psv"; export pop_pop11
 pop_prolog="$pop_pop11 -$popsavelib/prolog.psv"; export pop_prolog
 pop_clisp="$pop_pop11 -$popsavelib/clisp.psv"; export pop_clisp
 pop_pml="$pop_pop11 +$popsavelib/pml.psv"; export pop_pml
-pop_ved="$pop_po11 :sysinitcomp();ved"; export pop_ved
+pop_ved="$pop_pop11 :sysinitcomp();ved"; export pop_ved
 ****
 if [[ "$withXved" = "true" ]]; then
 cat >> "$popenv" <<\****
