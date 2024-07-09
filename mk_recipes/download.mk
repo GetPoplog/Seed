c44aa4ee9a12e765f194b5813e7c878c166cf666
Will Price <will.price94@gmail.com>
2024-07-09 20:30:10 +0100

    Disable SSL checks when downloading from cs.bham

--- mk_recipes/download.mk
+++ mk_recipes/download.mk
@@ -7,11 +7,11 @@ download: _download/packages-V$(MAJOR_VERSION).tar.bz2 _download/poplogout.sh _d
 
 _download/packages-V$(MAJOR_VERSION).tar.bz2:
 	mkdir -p "$(@D)"
-	curl -LsS "http://www.cs.bham.ac.uk/research/projects/poplog/V$(MAJOR_VERSION)/DL/packages-V$(MAJOR_VERSION).tar.bz2" > "$@"
+	curl -k -LsS "http://www.cs.bham.ac.uk/research/projects/poplog/V$(MAJOR_VERSION)/DL/packages-V$(MAJOR_VERSION).tar.bz2" > "$@"
 
 _download/poplogout.%:
 	mkdir -p "$(@D)"
-	curl -LsS "https://www.cs.bham.ac.uk/research/projects/poplog/V$(MAJOR_VERSION)/DL/$(notdir $@)" > "$@"
+	curl -k -LsS "https://www.cs.bham.ac.uk/research/projects/poplog/V$(MAJOR_VERSION)/DL/$(notdir $@)" > "$@"
 
 _download/appimagetool:
 	mkdir -p "$(@D)"
